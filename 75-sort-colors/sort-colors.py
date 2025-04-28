956ea74b406c73a6af7c454230098598a3a28773
Sreya Karuturi <81482118+sreya-518@users.noreply.github.com>
2025-04-28 22:03:07 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (83.36%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -3,15 +3,16 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        x = y = z = 0
-        for i in nums:
-            if i == 0:
-                x += 1
-            elif i ==1:
-                y +=1
+        low = mid = 0
+        high = len(nums) - 1
+
+        while mid <=high:
+            if nums[mid] == 0:
+                nums[low], nums[mid] = nums[mid], nums[low]
+                low +=1
+                mid +=1
+            elif nums[mid] == 1:
+                mid += 1
             else:
-                z +=1
-        nums[0:x] = [0] * x
-        nums[x:x+y] = [1] * y
-        nums[x+y:x+y+z] = [2] * z
-        
+                nums[high], nums[mid] = nums[mid], nums[high]
+                high -=1
