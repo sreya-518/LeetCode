3e4ec594152fdd99dc2b67ae9aa707ad02f924f4
Sreya Karuturi <81482118+sreya-518@users.noreply.github.com>
2025-05-03 21:24:27 +0530

    Time: 27 ms (5.17%) | Memory: 18 MB (19.42%) - LeetSync

--- 9-palindrome-number/palindrome-number.py
+++ 9-palindrome-number/palindrome-number.py
@@ -1,12 +1,25 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
         if x < 0:
-            return False 
+            return False
 
-        def reverse(num, rev=0):
-            if num == 0:
-                return rev
-            digit = num % 10
-            return reverse(num // 10, rev * 10 + digit)
+        # Find the divisor to extract the leftmost digit
+        def get_divisor(x):
+            div = 1
+            while x >= 10 * div:
+                div *= 10
+            return div
 
-        return x == reverse(x)
+        def helper(x, div):
+            if x == 0:
+                return True
+            left = x // div
+            right = x % 10
+            if left != right:
+                return False
+            # Remove the first and last digits
+            x = (x % div) // 10
+            div = div // 100
+            return helper(x, div)
+
+        return helper(x, get_divisor(x))
