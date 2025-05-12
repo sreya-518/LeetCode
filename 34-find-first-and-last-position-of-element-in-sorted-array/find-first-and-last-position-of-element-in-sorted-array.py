4fe5c2ca288124726fa9234e468b21dd057809ad
Sreya Karuturi <81482118+sreya-518@users.noreply.github.com>
2025-05-12 22:50:34 +0530

    Time: 0 ms (100.00%) | Memory: 19.1 MB (21.14%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,8 +1,55 @@
 import bisect
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        i = bisect.bisect_left(nums, target)
-        j = bisect.bisect_right(nums, target)
-        if i == j:
+        # i = bisect.bisect_left(nums, target)
+        # j = bisect.bisect_right(nums, target)
+        # if i == j:
+        #     return [-1, -1]
+        # return [i, j-1]
+
+        def firstOccurrence(arr, n, k):
+            low = 0
+            high = n - 1
+            first = -1
+
+            while low <= high:
+                mid = (low + high) // 2
+                if arr[mid] == k:
+                    first = mid
+                    high = mid - 1
+                elif arr[mid] < k:
+                    low = mid + 1 
+                else:
+                    high = mid - 1 
+            return first
+
+
+        def lastOccurrence(arr, n, k):
+            low = 0
+            high = n - 1
+            last = -1
+
+            while low <= high:
+                mid = (low + high) // 2
+                if arr[mid] == k:
+                    last = mid
+                    low = mid + 1
+                elif arr[mid] < k:
+                    low = mid + 1 
+                else:
+                    high = mid - 1
+
+            return last
+
+        def firstAndLastPosition(nums, n, k):
+            first = firstOccurrence(nums, n, k)
+            if first == -1:
+                return (-1, -1)
+            last = lastOccurrence(nums, n, k)
+            return (first, last)
+            
+        n = len(nums)
+        first, last = firstAndLastPosition(nums, n, target)
+        if first == -1:
             return [-1, -1]
-        return [i, j-1]        
+        return [first, last]
