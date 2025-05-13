ba10094b8cd0ee065e8e40d680dfaaf7a7422a0f
Sreya Karuturi <81482118+sreya-518@users.noreply.github.com>
2025-05-13 08:51:15 +0530

    Time: 0 ms (100.00%) | Memory: 18.4 MB (32.37%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.py
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.py
@@ -1,5 +1,7 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> bool:
+        #Time Complexity - Average & Best Case - O(log N)
+        #Worst Case - O(N/2)
         n = len(nums)
         low = 0
         high = n-1
@@ -22,7 +24,6 @@ class Solution:
                 if nums[mid] <= target and nums[high] >= target:
                     low = mid + 1
                 else:
-                    high = mid - 1
-            
+                    high = mid - 1         
         return False
             
