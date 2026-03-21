db248d0c740eb084df71837772b034ec42522ead
Sreya Karuturi <81482118+sreya-518@users.noreply.github.com>
2026-03-21 08:43:00 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (61.66%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.py
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -1,5 +1,6 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
+        #Start with the first word as the prefix and keep shortening it until it matches the beginning of every other word.
         res = ""
         prefix = strs[0]
         for s in strs[1:]: 
