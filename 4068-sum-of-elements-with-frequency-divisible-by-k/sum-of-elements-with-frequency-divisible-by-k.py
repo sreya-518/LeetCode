cf76cf4ae0511c4cf0d22f6a26b34b0d1af733ce
Sreya Karuturi <81482118+sreya-518@users.noreply.github.com>
2026-04-19 10:07:08 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (73.84%) - LeetSync

--- 4068-sum-of-elements-with-frequency-divisible-by-k/sum-of-elements-with-frequency-divisible-by-k.py
+++ 4068-sum-of-elements-with-frequency-divisible-by-k/sum-of-elements-with-frequency-divisible-by-k.py
@@ -1,22 +1,17 @@
+#Optimal Solution - Hash
 class Solution:
     def sumDivisibleByK(self, nums: List[int], k: int) -> int:
-        visitor = [0] * (10**4+1)
+        freq = {}
         result = 0
+
         for num in nums:
-            if visitor[num] == 0:
-                visitor[num] = 1
-                cnt = 0
-                for i in range(len(nums)):
-                    if num == nums[i]:
-                        cnt += 1
-                if cnt % k == 0:
-                    result += (num * cnt)
+            if num in freq:
+                freq[num] += 1
+            else:
+                freq[num] = 1
+        for num, cnt in freq.items():
+            if cnt % k == 0:
+                result += num * cnt
+
         return result
 
-
-
-
-
-
-
-        
