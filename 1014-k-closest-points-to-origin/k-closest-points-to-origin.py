b4b674c0b02892914036b9ef3d9caf1759df1314
Sreya Karuturi <81482118+sreya-518@users.noreply.github.com>
2026-07-28 08:46:15 +0530

    Time: 49 ms (83.29%) | Memory: 23.8 MB (94.71%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.py
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.py
@@ -1,16 +1,30 @@
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
-        minHeap = []
-        for x,y in points:
-            dist = (x ** 2) + (y ** 2)
-            minHeap.append([dist, x, y])
+        def distance(i):
+            x, y = points[i]
+            return x * x + y * y
+        def partition(left, right):
+            pivot_index = random.randint(left, right)
+            points[pivot_index], points[right] = points[right], points[pivot_index]
+            pivot_dist = distance(right)
+            p = left
+            for i in range(left, right):
+                if distance(i) <= pivot_dist:
+                    points[p], points[i] = points[i], points[p]
+                    p += 1
+            points[p], points[right] = points[right], points[p]
+            return p
 
-        heapq.heapify(minHeap)
-        res = []
-        while k > 0:
-            dist, x, y = heapq.heappop(minHeap)
-            res.append([x,y])
-            k -= 1
-        return res
+        left = 0
+        right = len(points) - 1
 
-        
+        while left <= right:
+            pivot = partition(left, right)
+            if pivot == k:
+                break
+            elif pivot < k:
+                left = pivot + 1
+            else:
+                right = pivot - 1
+
+        return points[:k]
